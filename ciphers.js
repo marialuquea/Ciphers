3f75fc4d7bfafad44a09f16e2ff8ec639b2dfd0a
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-02-12 09:03:16 +0000

    First commit
    Caesar done, but only works the first time yo use it. 
    Morse done but only to encipher. 
    Binary to do.
    About me needs more information.

---
+++ ciphers.js
@@ -0,0 +1,177 @@
+/*
+#####################################
+#            CAESAR CIPHER          #
+#              40280156             #
+#         MARIA LUQUE ANGUITA       #
+#####################################
+*/
+function caesarShift(){
+
+  var word = document.getElementById('txtBoxPlainText').value;
+  var amount = parseInt(document.getElementById('amount').value);
+
+  var output = '';
+
+  //Loop through each character
+  for (var i = 0; i < word.length; i++){
+
+    var letter = word[i];
+
+    var letter_code = letter.charCodeAt(0);
+
+    letter_code += amount;
+
+    letter = String.fromCharCode(letter_code);
+
+    output += letter;
+
+  }
+
+  console.log(output);
+
+  document.getElementById('txtBoxCipher').innerHTML = output;
+}
+
+
+function caesarDecipher(){
+
+  var word = document.getElementById('txtBoxCipher').value;
+  var amount = parseInt(document.getElementById('amount').value);
+
+  var output = '';
+
+  //Loop through each character
+  for (var i = 0; i < word.length; i++){
+
+    var letter = word[i];
+
+    var letter_code = letter.charCodeAt(0);
+
+    letter_code -= amount;
+
+    letter = String.fromCharCode(letter_code);
+
+    output += letter;
+
+  }
+
+  console.log(output);
+
+  //WHY DOESN'T THIS WORK?
+  document.getElementById('txtBoxPlainText').innerHTML = output;
+}
+
+
+
+/*
+#####################################
+#                                   #
+#            MORSE CIPHER           #
+#                                   #
+#####################################
+*/
+
+var morse = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+
+  " ": "   ", //spaces
+
+  ".": ".-.-.-",
+  ",": "--..--",
+  ":": "---...",
+  "?": "..--..",
+  "'": ".----.",
+  "-": "-....-",
+  "/": "-..-.",
+  "(": "-.--.-", //bracket signs are transmitted before and after the text affected
+  ")": "-.--.-", //that is why they both have the same code
+  "@": ".-.-.",
+  "=": "-...-",
+  "&": ".-...",
+  ";": "-.-.-.",
+  "+": ".-.-.",
+  "_": "..-.-",
+  "$": "...-..-",
+  "!": "-.-.-"
+};
+
+function morseCipher(){
+
+  var word = document.getElementById('PlainTextMorse').value;
+
+  var output = '';
+
+  word = word.toLowerCase().split("");
+
+  for (var i = 0; i < word.length; i++) {
+
+    word[i] = morse[word[i]];
+    output += word[i] + ' ';
+
+  }
+
+  console.log(output);
+
+  document.getElementById('CipherTextMorse').innerHTML = output;
+
+}
+
+//DOESN'T WORK YET
+function morseDecipher(){
+
+  var word = document.getElementById('CipherTextMorse').value;
+  console.log('1');
+
+  var output = '';
+  console.log('2');
+
+  word = word.split(" ");
+
+  for (var i = 0; i < word.length; i++) {
+
+    word[i] = morse[word[i]];
+    output += word[i];
+
+  }
+
+  console.log(output);
+
+  document.getElementById('PlainTextMorse').innerHTML = output;
+
+}
