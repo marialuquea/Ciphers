5de925b5740acd12553ed8ce6741bfee47c4dc4c
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-02-12 18:03:47 +0000

    caesar cipher now works perfectly
    still needs to decipher perfectly

--- ciphers.js
+++ ciphers.js
@@ -27,25 +27,29 @@ function caesarShift(){
 
       var letter = word[i];
 
+      //if it is a letter
       if (letter.match(/^[A-Za-z]+$/)){
 
         var letter_code = letter.charCodeAt(0);
 
-        letter_code += amount;
+          //ascii values of uppercase letters
+          if ((letter_code >= 65) && (letter_code <= 90)) {
+            //add amount without going into other ascii characters
+            letter_code = (((letter_code - 65 + amount) % 26) + 65);
+          }
 
+          //ascii values of lowercase letters
+          if ((letter_code >= 97) && (letter_code <=122)) {
+            letter_code = (((letter_code - 97 + amount) % 26) + 97)
+          }
+          
         letter = String.fromCharCode(letter_code);
-
       }
-
       output += letter;
-
     }
-
     console.log(output);
     document.getElementById('txtBoxCipher').innerHTML = output;
-
   }
-
 }
 
 
